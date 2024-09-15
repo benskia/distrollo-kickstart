00fa1352f6fe2733c5dec0724a7ba34efc218f9c
benskia <benski.abraham@gmail.com>
2024-09-15 15:48:14 -0500

    add dashboard plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,4 +10,15 @@ return {
             backdrop = 1,
         },
     } },
+
+    {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup {
+                -- config
+            }
+        end,
+        dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+    },
 }
