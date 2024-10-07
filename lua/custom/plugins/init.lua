626e79a6c4581ae08d3d6912e64da10755a77336
benskia <benski.abraham@gmail.com>
2024-10-07 13:03:21 -0500

    add .gitignore; add scrollEOF plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,22 +3,28 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    'tpope/vim-vinegar',
+	'tpope/vim-vinegar',
 
-    { 'folke/zen-mode.nvim', opts = {
-        window = {
-            backdrop = 1,
-        },
-    } },
+	{ 'folke/zen-mode.nvim', opts = {
+		window = {
+			backdrop = 1,
+		},
+	} },
 
-    {
-        'nvimdev/dashboard-nvim',
-        event = 'VimEnter',
-        config = function()
-            require('dashboard').setup {
-                -- config
-            }
-        end,
-        dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-    },
+	{
+		'nvimdev/dashboard-nvim',
+		event = 'VimEnter',
+		config = function()
+			require('dashboard').setup {
+				-- config
+			}
+		end,
+		dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+	},
+
+	{
+		'Aasim-A/scrollEOF.nvim',
+		event = { 'CursorMoved', 'WinScrolled' },
+		opts = {},
+	},
 }
